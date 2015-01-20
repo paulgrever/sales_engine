a4b05cb2c9b63f53d4808c4f9de1c85251fbfe8e
Paul Grever <paulgrever@gmail.com>
2015-01-20 13:41:32 -0700

    setting up merchant

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,4 +1,10 @@
 require 'csv'
 
 class Merchant
+
+  def initialize
+    merchant = CSV.open "./data/merchants.csv", headers: true, header_converters: :symbol
+  end
+  
 end
+
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/merchant'
+require 'csv'
+
 
 class MerchantTest < Minitest::Test
-  
+
   def test_it_exists
     assert Merchant
   end
