e386f39eb7b33db60542384e6990c0211fb285c5
Paul Grever <paulgrever@gmail.com>
2015-01-20 14:52:02 -0700

    created merchant class that stores id, name, created_at and Updated_at, also created merchant repository that creates an object for each merchant and passes the attributes

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,10 +1,15 @@
 require 'csv'
 
 class Merchant
+  attr_reader :id, :name, :created_at, :updated_at
 
-  def initialize
-    merchant = CSV.open "./data/merchants.csv", headers: true, header_converters: :symbol
+  def initialize(id, name, created_at, updated_at)
+    @id = id
+    @name = name
+    @created_at = created_at
+    @updated_at = updated_at
   end
-  
 end
 
+# test = Merchant.new("3", "paul", "9/20", "9/21")
+# puts test.name
---
+++ lib/merchant_repository.rb
@@ -0,0 +1,26 @@
+require 'csv'
+require './merchant'
+
+class MerchantRepository
+
+  def create_merchants
+    merchants = CSV.open '../data/merchants.csv', headers: true, header_converters: :symbol
+    merchants.map do |merchant|
+      id = merchant[:id]
+      name = merchant[:name]
+      created_at = merchant[:created_at]
+      updated_at = merchant[:updated_at]
+      Merchant.new(id, name, created_at, updated_at)
+    end
+  end
+
+
+
+end
+
+test = MerchantRepository.new
+john = test.create_merchants
+john.each do |merch|
+  puts "ID Number #{merch.id} is #{merch.name} they were created on #{merch.created_at}"
+end
+
