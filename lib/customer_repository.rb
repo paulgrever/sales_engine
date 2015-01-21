064d17e1e63ca6a4eb3aebd954aacd09b2955661
Paul Grever <paulgrever@gmail.com>
2015-01-21 16:53:26 -0700

    added customer repo playing around

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -1,7 +1,14 @@
+
 class CustomerRepository
 
   def initialize(filename)
-    @customer_list = 
+    @customer_list = CustomersParser.new(filename, self)
+    @calling_object = calling_object
   end
+
+  def init_parser(filename)
+    CustomersParser.new(filename, self)
+  end
+
 
 end
