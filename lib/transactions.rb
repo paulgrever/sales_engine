730ab7d167fc1ef817000a97502ceafa5c302cc2
Andrew Overby <andrewdoverby@gmail.com>
2015-01-21 14:25:23 -0700

    add transactions test

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -11,9 +11,4 @@ class Transactions
     @result = result
     @created_at = created_at
   end
-
-
 end
-
-# id,invoice_id,credit_card_number,credit_card_expiration_date,result,created_at,updated_at
-# 1,1,4654405418249632,,success,2012-03-27 14:54:09 UTC,2012-03-27 14:54:09 UTC
---
+++ test/transactions_test.rb
@@ -0,0 +1,48 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/transactions'
+require 'csv'
+
+
+class TransactionsTest < Minitest::Test
+  attr_reader :transactions
+
+  def setup
+    @transactions = Transactions.new("1","1", "4654405418249632","success","2012-03-27 14:54:09 UTC","2012-03-27 14:54:09 UTC")
+  end
+
+  def test_it_exists
+    assert Transactions
+  end
+
+  def test_it_takes_id
+    skip
+    assert_equal "1", transactions.id
+  end
+
+  def test_it_takes_invoice_id
+    skip
+    assert_equal "1", transactions.invoice_id
+  end
+
+  def test_it_takes_credit_card_number
+    skip
+    assert_equal "654405418249632", transactions.credit_card_number
+  end
+
+  def test_it_takes_result
+    skip
+    assert_equal "success", transactions,result
+  end
+
+  def test_it_was_created_at_certain_date
+    skip
+    assert_equal "2012-03-27 14:54:09 UTC", transactions.credit_card_expiration_date
+  end
+
+  def test_it_was_updated_at_certain_date
+    skip
+    assert_equal "2012-03-27 14:54:09 UTC", transactions.updated_at_certain_date
+  end
+
+end
