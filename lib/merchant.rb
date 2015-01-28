4318d4a7e2844e0828ed0d6dce47a8197be1e5a7
Paul Grever <paulgrever@gmail.com>
2015-01-28 11:06:13 -0700

    updated merchant so it passes relationship tests in harness

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -1,14 +1,23 @@
 require 'csv'
+require_relative 'sales_engine'
 
 class Merchant
-  attr_reader :id, :name, :created_at, :updated_at, :parent_class
+  attr_reader :id, :name, :created_at, :updated_at, :parent_engine
 
-  def initialize(id, name, created_at, updated_at, parent_class)
+  def initialize(id, name, created_at, updated_at, parent_engine)
     @id = id
     @name = name
     @created_at = created_at
     @updated_at = updated_at
-    @parent_class = parent_class
+    @parent_engine = parent_engine
+  end
+
+  def items
+    parent_engine.item_repository.find_all_by_merchant_id(@id)
+  end
+
+  def invoices
+    parent_engine.invoice_repository.find_all_by_merchant_id(@id)
   end
 end
 
